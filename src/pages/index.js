bd53ce037a24677187e7ae93076a44608e79beb9
Balázs Salfay <balazs.salfay@gmail.com>
2020-04-16 16:10:33 +0200

    favicon and website title modification

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Balazs Salfay | Portfolio'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Balazs Salfay | Portfolio'} />
       </Helmet>
       <App />
     </>
